5f340afe9b57f179229fc254a7825329b9788f79
ksheeley3 <ksheeley3@gmail.com>
2019-11-09 12:34:20 -0500

    added favorites searching

--- app/src/main/java/com/sytesapp/sytes/FavoriteActivity.java
+++ app/src/main/java/com/sytesapp/sytes/FavoriteActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,14 +15,17 @@ import java.util.ArrayList;
 public class FavoriteActivity extends AppCompatActivity {
 
     private RecyclerView.Adapter mAdapter;
-    public static ArrayList<String> displayedFavorites;
+    private ArrayList<String> displayedFavorites;
+    private ArrayList<String> searchList;
+    private String searchQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
 
-        displayedFavorites = MainActivity.currentFavorites;
+        searchList = (ArrayList)MainActivity.currentFavorites.clone();
+        displayedFavorites = (ArrayList)searchList.clone();
         RecyclerView favoritesView = findViewById(R.id.favoritesView);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
 
@@ -38,13 +42,44 @@ public class FavoriteActivity extends AppCompatActivity {
         favoritesView.setHasFixedSize(true);
         favoritesView.setLayoutManager(layoutManager);
         favoritesView.setAdapter(mAdapter);
+
+        final SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                searchQuery = query.toLowerCase();
+                searchInitialize();
+                mAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
     }
 
     @Override
     protected void onResume() {
-        displayedFavorites = MainActivity.currentFavorites;
+        searchList = (ArrayList)MainActivity.currentFavorites.clone();
+        searchInitialize();
         super.onResume();
         mAdapter.notifyDataSetChanged();
+    }
+
+    private void searchInitialize() {
+        displayedFavorites.clear();
+        for (int i = 0; i < searchList.size(); i++) {
+            if (searchQuery != null) {
+                if (searchList.get(i).split("\n")[1].toLowerCase().contains(searchQuery)) {
+                    displayedFavorites.add(searchList.get(i));
+                }
+            }
+            else {
+                displayedFavorites.add(searchList.get(i));
+            }
+        }
     }
 
     public void startMapActivity(View view) {
